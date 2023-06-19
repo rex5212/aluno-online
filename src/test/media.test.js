fcadb61cbc5215acdc6c6b1139796fe33090daf7
Renato Ostemberg <xrexv158925lpo@gmail.com>
2023-06-19 17:35:18 -0300

    Make the all test pass

--- src/test/media.test.js
+++ src/test/media.test.js
@@ -6,18 +6,18 @@ describe("TESTE", () => {
         expect(resultado.mediaCA(9.1)).toEqual(`Sua media final foi 9.1 com pontuação SS (Superior)`)
     });
     test("Teste Nota Médio Superior", () => {
-        expect(resultado.mediaCA(7.0)).toEqual(`Sua media final foi 7.0 com pontuação MS (Médio Superior)`);
+        expect(resultado.mediaCA(7.0)).toEqual(`Sua media final foi 7 com pontuação MS (Médio Superior)`);
     });
     test("Teste Nota Médio", () => {
-        expect(resultado.mediaCA(5.0)).toEqual(`Sua media final foi 5.0 com pontuação MM (Médio)`);
+        expect(resultado.mediaCA(5.0)).toEqual(`Sua media final foi 5 com pontuação MM (Médio)`);
     });
     test("Teste Nota Médio Inferior", () => {
-        expect(resultado.mediaCA(3.0)).toEqual(`Sua media final foi 3.0 com pontuação MI (Médio Inferior)`);
+        expect(resultado.mediaCA(3.0)).toEqual(`Sua media final foi 3 com pontuação MI (Médio Inferior)`);
     });
     test("Teste Nota Inferior", () => {
         expect(resultado.mediaCA(0.1)).toEqual(`Sua media final foi 0.1 com pontuação II (Inferior)`);
     });
     test("Teste Nota Sem rendimento", () => {
-        expect(resultado.mediaCA(0)).toEqual(`Sua media final foi 0.0 com pontuação SR (Sem rendimento)`);
+        expect(resultado.mediaCA(0)).toEqual(`Sua media final foi 0 com pontuação SR (Sem rendimento)`);
     });
 });
